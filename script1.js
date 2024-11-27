d66af68bd735c3784bdf64294040860cfe56b86b
ShreeSundaram <srideviire@gmail.com>
2024-11-27 18:14:36 +0000

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+
+const number = [12, 45, 2, 89, 33, 90, 4, 77];
+
+const largestNumber = Math.max(...number);
+
+
+console.log(`The largest number in the array is: ${largestNumber}`);
+alert(`The largest number in the array is: ${largestNumber}`);
